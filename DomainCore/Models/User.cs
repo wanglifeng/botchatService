9db0a5a677622996a802360418c381ed3138fc89
wanglifeng <tytc011@163.com>
2013-03-05 01:55:01 +0800

    pass mstests

--- DomainCore/Models/User.cs
+++ DomainCore/Models/User.cs
@@ -5,10 +5,14 @@ using System.Text;
 
 namespace DomainCore.Models
 {
-    class User
+    public class User
     {
         public int Id { get; set; }
         public String UserName { get; set; }
+        public String ClientId { get; set; }
+        public String Location { get; set; }
+        public String Name { get; set; }
+        public String Degree { get; set; }
 
     }
 }
